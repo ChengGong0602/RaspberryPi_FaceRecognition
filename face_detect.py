9b6adb35751b5d3e5b87646f15f1f56bd9b0eadf
ChengGong0602 <rjs0602@163.com>
2022-08-30 23:24:29 +0800

    trigger integration

--- face_detect.py
+++ face_detect.py
@@ -1,5 +1,68 @@
+from turtle import xcor
 import cv2
 import mediapipe as mp
+import math
+from time import sleep
+from datetime import datetime
+from sh import gphoto2 as gp
+import signal, os, subprocess
+
+# Kill the gphoto process that starts
+# whenever we turn on the camera or
+# reboot the raspberry pi
+
+
+def killGphoto2Process():
+    p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+
+    # Search for the process we want to kill
+    for line in out.splitlines():
+        if b'gvfsd-gphoto2' in line:
+            # Kill that process!
+            pid = int(line.split(None,1)[0])
+            os.kill(pid, signal.SIGKILL)
+
+shot_date = datetime.now().strftime("%Y-%m-%d") # This has been written to the while True loop.
+# shot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # This has been written to the while True loop.
+picID = "PiShots"
+
+clearCommand = ["--folder", "/store_00020001/DCIM/100CANON", \
+                "--delete-all-files", "-R"]
+triggerCommand = ["--trigger-capture"]
+downloadCommand = ["--get-all-files"]
+
+folder_name = shot_date + picID
+save_location = "/home/pi/Desktop/gphoto/images/" + folder_name
+
+def createSaveFolder():
+    try:
+        os.makedirs(save_location)
+    except:
+        print("Failed to create new directory.")
+    os.chdir(save_location)
+
+def captureImages():
+    gp(triggerCommand)
+    sleep(3)
+    gp(downloadCommand)
+    gp(clearCommand)
+
+def renameFiles(ID):
+    for filename in os.listdir("."):
+        if len(filename) < 13:
+            if filename.endswith(".JPG"):
+                os.rename(filename, (shot_time + ID + ".JPG"))
+                print("Renamed the JPG")
+            elif filename.endswith(".CR2"):
+                os.rename(filename, (shot_time + ID + ".CR2"))
+                print("Renamed the CR2")
+
+
+killGphoto2Process()
+gp(clearCommand)
+
+ 
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 
@@ -27,28 +90,64 @@ with mp_face_detection.FaceDetection(
 # For webcam input:
 cap = cv2.VideoCapture(0)
 with mp_face_detection.FaceDetection(
-    model_selection=0, min_detection_confidence=0.5) as face_detection:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+        model_selection=0, min_detection_confidence=0.5) as face_detection:
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = face_detection.process(image)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = face_detection.process(image)
+        image_rows, image_cols, _ = image.shape
 
-    # Draw the face detection annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.detections:
-      for detection in results.detections:
-        mp_drawing.draw_detection(image, detection)
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Face Detection', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
+        # Draw the face detection annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.detections:
+            all_look_forward = True
+            for detection in results.detections:
+               
+                # mp_drawing.draw_detection(image, detection)
+                location = detection.location_data
+                if location.format != mp.solutions.drawing_utils.location_data_pb2.LocationData.RELATIVE_BOUNDING_BOX:
+                    raise ValueError( 'LocationData must be relative for this drawing funtion to work.')
+                # Draws keypoints.
+                xy_points = []
+                count = 0
+                for keypoint in location.relative_keypoints:
+                    count = count + 1
+                    # print("keypoint", keypoint)
+                    keypoint_px = mp.solutions.drawing_utils._normalized_to_pixel_coordinates(keypoint.x, keypoint.y, image_cols, image_rows)
+                    # cv2.circle(image, keypoint_px, 1, (255,5,255), 2)
+                    # print("keypoint_px", keypoint_px)
+                    xy_points.append(keypoint_px)
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    cv2.putText(image, str(count), keypoint_px, font, 1, (255, 5, 255), 2, cv2.LINE_AA)
+                    # right eye, left eye, nose tip, mouth center, right ear tragion, and left ear tragion
+                # print("xy_points", xy_points[5][1])
+                distance3_5 = math.dist(xy_points[2], xy_points[4])
+                distance3_6 = math.dist(xy_points[2], xy_points[5])                
+                if (xy_points[5][0] < xy_points[1][0] or xy_points[4][0] > xy_points[0][0]):
+                    all_look_forward = False
+                else:
+                    all_look_forward = True               
+            # all look forward
+            if all_look_forward  == True:
+                print("Take a photo")
+                shot_date = datetime.now().strftime("%Y-%m-%d")
+                shot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                createSaveFolder()
+                captureImages()
+                renameFiles(picID)
+            else:
+                print("Doesn't take a photo")
+        # Flip the image horizontally for a selfie-view display.
+        cv2.imshow('MediaPipe Face Detection', cv2.flip(image, 1))
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
 cap.release()
--- test.py
+++ test.py
@@ -1,79 +1,67 @@
+from turtle import xcor
 import cv2
 import mediapipe as mp
+import math
+
+mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_pose = mp.solutions.pose
 
-'''
-# For static images:
-IMAGE_FILES = []
-BG_COLOR = (192, 192, 192) # gray
-with mp_pose.Pose(
-    static_image_mode=True,
-    model_complexity=2,
-    enable_segmentation=True,
-    min_detection_confidence=0.5) as pose:
-  for idx, file in enumerate(IMAGE_FILES):
-    image = cv2.imread(file)
-    image_height, image_width, _ = image.shape
-    # Convert the BGR image to RGB before processing.
-    results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-    if not results.pose_landmarks:
-      continue
-    print(
-        f'Nose coordinates: ('
-        f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
-        f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height})'
-    )
-
-    annotated_image = image.copy()
-    # Draw segmentation on the image.
-    # To improve segmentation around boundaries, consider applying a joint
-    # bilateral filter to "results.segmentation_mask" with "image".
-    condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
-    bg_image = np.zeros(image.shape, dtype=np.uint8)
-    bg_image[:] = BG_COLOR
-    annotated_image = np.where(condition, annotated_image, bg_image)
-    # Draw pose landmarks on the image.
-    mp_drawing.draw_landmarks(
-        annotated_image,
-        results.pose_landmarks,
-        mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
-    # Plot pose world landmarks.
-    mp_drawing.plot_landmarks(
-        results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
-'''
 # For webcam input:
 cap = cv2.VideoCapture(0)
-with mp_pose.Pose(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as pose:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+with mp_face_detection.FaceDetection(
+        model_selection=0, min_detection_confidence=0.5) as face_detection:
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = pose.process(image)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = face_detection.process(image)
+        image_rows, image_cols, _ = image.shape
 
-    # Draw the pose annotation on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    mp_drawing.draw_landmarks(
-        image,
-        results.pose_landmarks,
-        mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
+        # Draw the face detection annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.detections:
+            all_look_forward = True
+            for detection in results.detections:
+               
+                # mp_drawing.draw_detection(image, detection)
+                location = detection.location_data
+                if location.format != mp.solutions.drawing_utils.location_data_pb2.LocationData.RELATIVE_BOUNDING_BOX:
+                    raise ValueError( 'LocationData must be relative for this drawing funtion to work.')
+                # Draws keypoints.
+                xy_points = []
+                count = 0
+                for keypoint in location.relative_keypoints:
+                    count = count + 1
+                    # print("keypoint", keypoint)
+                    keypoint_px = mp.solutions.drawing_utils._normalized_to_pixel_coordinates(keypoint.x, keypoint.y, image_cols, image_rows)
+                    # cv2.circle(image, keypoint_px, 1, (255,5,255), 2)
+                    # print("keypoint_px", keypoint_px)
+                    xy_points.append(keypoint_px)
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    cv2.putText(image, str(count), keypoint_px, font, 1, (255, 5, 255), 2, cv2.LINE_AA)
+                    # right eye, left eye, nose tip, mouth center, right ear tragion, and left ear tragion
+                # print("xy_points", xy_points[5][1])
+                distance3_5 = math.dist(xy_points[2], xy_points[4])
+                distance3_6 = math.dist(xy_points[2], xy_points[5])                
+                if (xy_points[5][0] < xy_points[1][0] or xy_points[4][0] > xy_points[0][0]):
+                    all_look_forward = False
+                else:
+                    all_look_forward = True               
+            # all look forward
+            if all_look_forward  == True:
+                print("Take a photo")
+            else:
+                print("Doesn't take a photo")
+        # Flip the image horizontally for a selfie-view display.
+        cv2.imshow('MediaPipe Face Detection', cv2.flip(image, 1))
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
 cap.release()
