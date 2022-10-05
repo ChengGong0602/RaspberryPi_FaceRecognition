96b31ed16f3219d14891e5bdc83a9dd33f1f79a3
ChengGong0602 <rjs0602@163.com>
2022-10-05 16:26:33 +0800

    update with python-gphoto2

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from time import sleep
 from datetime import datetime
-from sh import gphoto2 as gp
+# from sh import gphoto2 as gp
 import signal, os, subprocess
+import gphoto2 as gp
 
 
 clearCommand = ["--folder", "/store_00020001/DCIM/100CANON", \
@@ -31,4 +32,37 @@ def killGphoto2Process():
 
 
 killGphoto2Process()
-gp(getConfigCommand)
+# gp(getConfigCommand)
+
+
+context = gp.Context()
+camera = gp.Camera()
+camera.init(context)
+config_tree = camera.get_config(context)
+print('=======')
+
+total_child = config_tree.count_children()
+for i in range(total_child):
+    child = config_tree.get_child(i)
+    text_child = '# ' + child.get_label() + ' ' + child.get_name()
+    print(text_child)
+
+    for a in range(child.count_children()):
+        grandchild = child.get_child(a)
+        text_grandchild = '    * ' + grandchild.get_label() + ' -- ' + grandchild.get_name()
+        print(text_grandchild)
+
+        try:
+            text_grandchild_value = '        Setted: ' + grandchild.get_value()
+            print(text_grandchild_value)
+            print('        Possibilities:')
+            for k in range(grandchild.count_choices()):
+                choice = grandchild.get_choice(k)
+                text_choice = '         - ' + choice
+                print(text_choice)
+        except:
+            pass
+        print()
+    print()
+
+camera.exit(context)
