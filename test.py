bac86e15376f48dc5c6bbe49807ee71d13df83a0
ChengGong0602 <rjs0602@163.com>
2022-10-06 01:52:24 +0800

    config

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from time import sleep
 from datetime import datetime
-# from sh import gphoto2 as gp
+from sh import gphoto2 as gp
 import signal, os, subprocess
-import gphoto2 as gp
+# import gphoto2 as gp
 
 
 clearCommand = ["--folder", "/store_00020001/DCIM/100CANON", \
@@ -27,42 +27,42 @@ def killGphoto2Process():
             pid = int(line.split(None,1)[0])
             os.kill(pid, signal.SIGKILL)
 
-# def get_configuration():
+def get_all_configuration():
+    context = gp.Context()
+    camera = gp.Camera()
+    camera.init(context)
+    config_tree = camera.get_config(context)
+    print('=======')
 
+    total_child = config_tree.count_children()
+    for i in range(total_child):
+        child = config_tree.get_child(i)
+        text_child = '# ' + child.get_label() + ' ' + child.get_name()
+        print(text_child)
+
+        for a in range(child.count_children()):
+            grandchild = child.get_child(a)
+            text_grandchild = '    * ' + grandchild.get_label() + ' -- ' + grandchild.get_name()
+            print(text_grandchild)
+
+            try:
+                text_grandchild_value = '        Setted: ' + grandchild.get_value()
+                print(text_grandchild_value)
+                print('        Possibilities:')
+                for k in range(grandchild.count_choices()):
+                    choice = grandchild.get_choice(k)
+                    text_choice = '         - ' + choice
+                    print(text_choice)
+            except:
+                pass
+            print()
+    print()
+
+    camera.exit(context)    
 
 
 killGphoto2Process()
-# gp(getConfigCommand)
+gp(triggerCommand)
+gp(getConfigCommand)
 
 
-context = gp.Context()
-camera = gp.Camera()
-camera.init(context)
-config_tree = camera.get_config(context)
-print('=======')
-
-total_child = config_tree.count_children()
-for i in range(total_child):
-    child = config_tree.get_child(i)
-    text_child = '# ' + child.get_label() + ' ' + child.get_name()
-    print(text_child)
-
-    for a in range(child.count_children()):
-        grandchild = child.get_child(a)
-        text_grandchild = '    * ' + grandchild.get_label() + ' -- ' + grandchild.get_name()
-        print(text_grandchild)
-
-        try:
-            text_grandchild_value = '        Setted: ' + grandchild.get_value()
-            print(text_grandchild_value)
-            print('        Possibilities:')
-            for k in range(grandchild.count_choices()):
-                choice = grandchild.get_choice(k)
-                text_choice = '         - ' + choice
-                print(text_choice)
-        except:
-            pass
-        print()
-    print()
-
-camera.exit(context)
